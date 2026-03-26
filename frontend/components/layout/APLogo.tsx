ed09fde2b23784d9bf0739d40de97668c3e32018
Billy <641833868@qq.com>
2026-03-26 20:39:53 +0800

    feat: add APLogo component for sidebar

---
+++ frontend/components/layout/APLogo.tsx
@@ -0,0 +1,22 @@
+// frontend/components/layout/APLogo.tsx
+export default function APLogo() {
+  return (
+    <div
+      style={{
+        width: 28,
+        height: 28,
+        background: '#0f766e',
+        borderRadius: 7,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: '#fff',
+        fontWeight: 800,
+        fontSize: 12,
+        flexShrink: 0,
+      }}
+    >
+      AP
+    </div>
+  );
+}
